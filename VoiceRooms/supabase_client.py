e063b5edb5d2325d8f5acab8b59668df07607732
elmechtasaber-beep <your_email@gmail.com>
2026-08-29 03:32:13 +0100

    fix duplicate supabase url in VoiceRooms

--- VoiceRooms/supabase_client.py
+++ VoiceRooms/supabase_client.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 
-SUPABASE_URL = "https://kwlovnyznahfkyhvmyzv.supabase.co"
+SUPABASE_URL = "https://kwlcvnyznahfkyfvmymw.supabase.co"
 SUPABASE_KEY = "sb_publishable_pzWRXTsydWX_FoyWnF_Tmg_5VWO-vKd"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
